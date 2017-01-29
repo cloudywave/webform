931bdbef472183369eb38653f5d95ceae16ce36d
cloudywave <support@cloudywave.com>
2017-01-29 11:47:08 +0530

    Update webform.js

--- webform.js
+++ webform.js
@@ -1,16 +1,16 @@
 
         function submitForm() {
-var account ={};           $( "[ID^='Account']" ).each(function(key,value){ debugger; account[$(value).attr('id')] =$(value).val();    })
-         
-            $.ajax({
+var data ={};           $( "[ID^='Account']" ).each(function(key,value){ debugger; account[$(value).attr('id')] =$(value).val();    })
+         var account={}; $.ajax({
                 url: 'https://webapitest-developer-edition.eu6.force.com/services/apexrest/WebForms',
-                type: "POST",
-                dataType: "json", headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json; charset=UTF-8' }, data:JSON.stringify( {"jsondata": account}),
+                type: "POST",  contentType: 'application/json',
+                dataType: "json", headers: { 'Access-Control-Allow-Origin': '*' }, data:   JSON.stringify({"prakash":[]}),
                 success: function (result) {
-                   
+                  console.log(result);
                 },
                 error: function (xhr, ajaxOptions, thrownError) {
                     console.log(xhr);
                 }
             });
+      
         }
